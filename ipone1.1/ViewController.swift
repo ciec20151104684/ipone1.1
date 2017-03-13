d559f5d68ca8cea0117a6d65e11424d4ff784b4c
ciec20151104684 <s20151104684@King.local>
2017-03-13 12:04:00 +0800

    新计算器部分功能的实现

--- ipone1.1/ViewController.swift
+++ ipone1.1/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var input: UITextField!
+    
+    @IBOutlet weak var output: UITextField!
+    
+    @IBAction func zero(_ sender: Any) {
+        let temp0: Int = 0
+        input.text=(input.text!+("\(temp0)"))
+    }
+    
+    @IBAction func one(_ sender: Any) {
+        let temp1: Int = 1
+        input.text=(input.text!+("\(temp1)"))
+    }
+    
+    @IBAction func two(_ sender: Any) {
+        let temp2: Int = 2
+        input.text=(input.text!+("\(temp2)"))
+    }
+    
+   
+    
+    
+    @IBAction func suanfa(_ sender: UIButton) {
+        let an = sender.currentTitle
+       if an=="+"||an=="-"||an=="*"||an=="/"
+       {
+        input.text = an
+        }
+    }
+    
+   
+    @IBAction func area(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
